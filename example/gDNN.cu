0827596bb251ea63e8be46743537d80d2adb9b53
Xiaoyun Wang <wangxiaoyunanne@gmail.com>
2019-06-11 19:47:19 -0700

    add category.tsv

--- example/gDNN.cu
+++ example/gDNN.cu
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <fstream>
 
 #include <cstdio>
 #include <cstdlib>
@@ -12,7 +13,7 @@
 #include <boost/program_options.hpp>
 
 #include "graphblas/graphblas.hpp"
-#include "graphblas/algorithm/DNN.hpp"
+//#include "graphblas/algorithm/DNN.hpp"
 #include "graphblas/algorithm/common.hpp"
 //#include "test/test.hpp"
 
@@ -23,8 +24,10 @@ int main(int argc, char** argv) {
   std::vector<graphblas::Index> row_indices, row_idx_mnist;
   std::vector<graphblas::Index> col_indices, col_idx_mnist;
   std::vector<float> values, val_mnist;
+  std::vector<int> true_categories_idx;
+  std::vector<bool> true_categories(60000,0);
   graphblas::Index nrows, ncols, nvals, nrow_mnist, ncol_mnist, nval_mnist;
-
+   
   // Parse arguments
   bool debug;
   bool transpose;
@@ -54,19 +57,21 @@ int main(int argc, char** argv) {
      */
     // read layers
     //std :: string mnist_file = "/home/wangxy/GraphChallenge/code/data/MNIST/sparse-images-1024.mtx" ;
-    //std :: string cate_file = "/home/wangxy/GraphChallenge/code/data/DNN/neuron1024-l120-categories.mtx";
+    //std :: string cate_file = "/home/wangxy/GraphChallenge/code/data/DNN/neuron1024-l120-categories.tsv";
+    std :: ifstream categ_file;
+    categ_file.open (argv[argc-3]);
+    int x;
+    while (categ_file >> x) {
+        true_categories_idx.push_back(x);
+        true_categories[x-1] = 1;
+    }
+    //std :: cout<<categories.size() <<std :: endl;
+    
+
     readMtx(argv[argc-2], &row_idx_mnist, &col_idx_mnist, &val_mnist, &nrow_mnist, &ncol_mnist,
         &nval_mnist, directed, mtxinfo, NULL);
-    /*
-    graphblas::Matrix<float> mnist(nrow_mnist, ncol_mnist);
-    CHECK(mnist.build(&row_idx_mnist, &col_idx_mnist, &val_mnist, nval_mnist, GrB_NULL,
-      NULL));
-    CHECK(mnist.nrows(&nrows));
-    CHECK(mnist.ncols(&ncols));
-    CHECK(mnist.nvals(&nvals));
-    if (debug) CHECK(mnist.print());
-    std::cout << "#mnist values = " << nval_mnist << std::endl;    
-    */
+    
+   
     for (int layer = 0; layer < nlayers; layer ++ )
     {
         std :: string file_name = std :: string(argv[argc-1]) + "n1024-l";
@@ -106,12 +111,20 @@ int main(int argc, char** argv) {
   CHECK(mnist.nvals(&nvals));
   if (debug) CHECK(mnist.print());
   std::cout << "#mnist values = " << nval_mnist << std::endl;
+
+  // read categories to 
+
+ 
+ 
+
   graphblas::Matrix<float> y(nrow_mnist, ncol_mnist);
   y = mnist ;
-  for (int layer = 0; layer < nlayers; layer++ )
-  {
-      y = graphblas ::algorithm :: DNN (&y, &Weights[layer]);
-  }
+  //for (int layer = 0; layer < nlayers; layer++ )
+  //{
+  //    y = graphblas ::algorithm :: DNN (&y, &Weights[layer]);
+  //}
+
+  
   
 
 
