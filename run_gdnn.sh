e4879c82f6cf9f3b88bf3459c6a06203bee2e888
Carl Yang <ctcyang@ucdavis.edu>
2019-06-17 13:13:53 -0700

    add environmental variable configurations to run script

--- run_gdnn.sh
+++ run_gdnn.sh
@@ -1,3 +1,5 @@
+export GRB_SPARSE_MATRIX_FORMAT=1
+export GRB_UTIL_REMOVE_SELFLOOP=0
 ./bin/gdnn --directed 0 --debug 1 \
 /home/wangxy/GraphChallenge/code/data/DNN/neuron1024-l120-categories.tsv \
 /home/wangxy/GraphChallenge/code/data/MNIST/sparse-images-1024.mtx \
